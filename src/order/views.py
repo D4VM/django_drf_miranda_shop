a96f18e89321d69ed092f7aa3e9597d15376a63f
D4VM <david.miru@gmail.com>
2024-05-12 13:19:16 +0400

    drf docs

--- src/order/views.py
+++ src/order/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
-from .models import Order, OrderItems
+from .models import Order
 from .serializers import OrderSerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Заказы"])
 class OrderListApiView(generics.ListAPIView, generics.CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
